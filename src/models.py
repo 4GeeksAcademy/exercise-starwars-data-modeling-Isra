402a3d1bbc3e1b08f089805a341461f0d3ec947f
Israel Sánchez <israelclarinete@gmail.com>
2024-07-02 08:35:45 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -1,32 +1,69 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(100), nullable=False)
+    password = Column(String(50), nullable=False)
+    email = Column(String(100), nullable=False)
+    favorites = relationship("Favorite")
 
-    def to_dict(self):
-        return {}
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    gender =Column(Enum('female', 'male', 'other', 'n/a'))
+    birth_year = Column(Integer)
+    height = Column(Integer)
+    hair_color = Column(Enum('brown', 'blond', 'red', 'black', 'n/a'))
+    eye_color = Column(Enum('brown', 'green', 'blue', 'gold', 'n/a'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet= relationship("Planet")
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    population = Column(Integer)
+    climate = Column(Enum('arid', 'temperate', 'tropical', 'frozen', 'murky'))
+    terrain = Column(Enum('desert', 'grasslands', 'mountains', 'forests', 'rainforests','jungle', 'ocean', 'tundra','ice caves','ranges','swamps','gas gigant','lakes','grassy hills','cityscape'))
+    orbital_period = Column(Integer)
+    rotation_period = Column(Integer)
+    diameter = Column(Integer)
+    characters = relationship("Character")
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name= Column(String(100), nullable=False)
+    model= Column(String(100), nullable=False)
+    vehicle_class=Column(Enum('repulsorcraft', 'wheeled', 'starfighter'))
+    manufacturer= Column(Enum('Incom Corporation', 'Corellia Mining Corporation'), nullable=False)
+    length= Column(Integer)
+    passengers= Column(Integer)
+    pilot_id= Column(Integer, ForeignKey('character.id'))
+    character= relationship("Character")
+
+class Favorites(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    user= relationship("User")
+    character= relationship("Character")
+    planet= relationship("Planet")
+    vehicle= relationship("Vehicle")
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
